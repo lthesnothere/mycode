58a75224de3677bebc26eb85d5029dcbbf9b7e5c
lthesnothere <lthesnothere@users.noreply.github.com>
2021-10-06 19:41:52 +0000

    studying for logic

--- ghbackup/ghbackup.py
+++ ghbackup/ghbackup.py
@@ -25,7 +25,8 @@ def scan_for_arguments():
   argumentList = sys.argv[1:]
   options      = "v" 
   long_options = ["version"]
-  version      = '1.0'
+#  comment      = input()
+  version      = '1.1'
   try:
     arguments, values = getopt.getopt(argumentList, options, long_options)
     for currentArgument, currentValue in arguments:
@@ -33,6 +34,7 @@ def scan_for_arguments():
         print (os.path.basename(sys.argv[0]), version)
   except:
     print("Error 101")
+  sys.exit()
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def run_git_commands():
 #  os.system("cd ~/mycode")
