38f529ed8d9a52f226de7684bc199ac5689ee0f4
lthesnothere <lthesnothere@users.noreply.github.com>
2021-10-07 21:05:48 +0000

    Python and physical interfaces

---
+++ netfacd/interface_reader.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import netifaces
+print(netifaces.interfaces())
+for i in netifaces.interfaces():
+    print('\n**************Details of Interface - ' + i + ' *********************')
+#    print(netifaces.ifaddresses(i))
+#    print(netifaces.ifaddresses(i)[netifaces.AF_LINK])
+    try:
+        print('MAC:  ', end="")
+        print(netifaces.ifaddresses(i)[netifaces.AF_LINK][0]['addr'])
+        print('IP:   ', end="")
+        print(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+    except:
+        print('Could not collect adaptor information') #Prints an error message.
