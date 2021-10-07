580d60070ff5a00ce707b81e399331c9a33b59a3
lthesnothere <lthesnothere@users.noreply.github.com>
2021-10-07 01:03:27 +0000

    Learning to archive files

---
+++ cfgread/cfg03.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+## create file object in "r"ead mode
+with open("vlanconfig.cfg", "r") as configfile:
+    ## readlines() creates a list by reading target
+    ## file line by line
+    configlist = configfile.readlines()
+    num_lines = sum(1 for line in open("vlanconfig.cfg"))
+#    configprint = configlist.splitlines()
+## file was just auto closed (no more indenting)
+
+## each item of the list now has the "\n" characters back
+print(configlist)
+print(num_lines)
+#print(configprint)
+
