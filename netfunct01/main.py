2b25e50c0f5543488e784dedcad08c5ae3b89437
lthesnothere <lthesnothere@users.noreply.github.com>
2021-10-07 20:06:15 +0000

    Learning about functions

---
+++ netfunct01/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author: RZFeeser@alta3.com"""
+# Importing Python3 Crayons
+import crayons
+
+# function to push commands
+def commandpush(devicecmd): # devicecmd==dict
+
+    for ip in devicecmd.keys(): # looping through the dict
+        print(f'{crayons.yellow("Handshaking")}. .. ... connecting with {ip}') # fstring with Yellow
+        # we'll learn to write code that connects to devices here
+        for mycmds in devicecmd[ip]:
+            print(f'{crayons.red("Attempting to sending command -->")} {mycmds}')
+            # we'll learn to write code that sends cmds to device here
+    return None
+
+# start our main script
+def main():
+    """called at runtime"""
+
+    # dict containing IPs mapped to a list of physical interfaces and their state
+    devicecmd = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1":
+    ["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]}
+
+    print(f"\n\nWelcome ...\nto the \n{crayons.blue('Network')} \n{crayons.green('Device')} \n{crayons.yellow('Command')} \n{crayons.cyan('Pusher')}") # welcome message
+#    print(f"Welcome to the {crayons.blue('Network')} device command pusher") # welcome message
+
+    ## get data set
+    print("\nData set found\n") # replace with function call that reads in data from file
+
+    ## run
+    commandpush(devicecmd) # call function to push commands to devices
+
+# call our main function
+main()
+
